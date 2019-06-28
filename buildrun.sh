b38ce5baef56c176a38966fe9247af9f71dc50c7
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-06-28 16:22:11 +0100

    missing porty

--- buildrun.sh
+++ buildrun.sh
@@ -1,2 +1,2 @@
 docker build -t sonia-glassfish .
-docker run --rm -ti -p 4848:4848 -p 8080:8080  -v glassfish:/usr/local/glassfish4  sonia-glassfish 
+docker run --rm -ti -p 4848:4848 -p 8080:8080 -p 8181:8181 -v glassfish:/usr/local/glassfish4  sonia-glassfish 
