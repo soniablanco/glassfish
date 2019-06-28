cb3b068132e47d5cfe8432cefc4bd3ad10c823b5
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-06-28 16:03:36 +0100

    saving state

--- buildrun.sh
+++ buildrun.sh
@@ -1,2 +1,2 @@
 docker build -t sonia-glassfish .
-docker run --rm -ti -p 4848:4848 -p 8080:8080 sonia-glassfish
+docker run --rm -ti -p 4848:4848 -p 8080:8080  -v ~/glass/home:/usr/local/glassfish4  sonia-glassfish 
